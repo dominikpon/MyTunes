382a38fdc72eff33bdd9d90b275dbf2fae05385b
dominikpon <doominikponik@gmail.com>
2025-11-24 10:58:40 +0100

    Initial
    Initial

---
+++ src/main/java/dk/easv/cs5/mytunes/Launcher.java
@@ -0,0 +1,7 @@
+package dk.easv.cs5.mytunes;
+
+public class Launcher {
+    public static void main(String[] args) {
+        javafx.application.Application.launch(Application.class, args);
+    }
+}
