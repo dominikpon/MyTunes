3838f0b48a4283cd6dff890695d4e6807796d708
dominikpon <doominikponik@gmail.com>
2025-11-25 10:48:05 +0100

    Add Playlist and Song BE, update app title
    Implemented Playlist and Song classes with relevant fields and methods to support application data models. Changed the application window title to 'MyTunes'.

--- src/main/java/dk/easv/cs5/mytunes/be/Playlist.java
+++ src/main/java/dk/easv/cs5/mytunes/be/Playlist.java
@@ -1,6 +1,26 @@
 package dk.easv.cs5.mytunes.be;
 
 public class Playlist {
+    private int id;
+    private String name;
+    private String songs;
+    private int duration;
 
-
+    public Playlist(int id, String name, int duration) {
+        this.id = id;
+        this.name = name;
+        this.duration = duration;
+    }
+    public Playlist(String title){
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/dk/easv/cs5/mytunes/be/Song.java
+++ src/main/java/dk/easv/cs5/mytunes/be/Song.java
@@ -4,7 +4,38 @@ public class Song {
     private int id;
     private String title;
     private String artist;
-    private String album;
+    private String genre;
     private int duration;
+    private String filePath;
 
+    public Song(int id, String title, String artist, String genre, int duration) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.duration = duration;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public String getDurationInSeconds() {  // Converting and formatting duration
+        int minutes = duration / 60;
+        int seconds = duration % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+    public String toString() {
+        return "ID: "+id+" Title: "+title+" Artist: "+artist+" Genre: "+genre;
+    }
 }
