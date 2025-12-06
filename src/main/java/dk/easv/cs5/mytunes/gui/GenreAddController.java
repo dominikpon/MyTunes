ee191396f7bcbb8a5e4aac78d6461275b2e3f5e7
hlacis <jakubhlacik@seznam.cz>
2025-12-06 11:49:56 +0100

    Added genre window

---
+++ src/main/java/dk/easv/cs5/mytunes/gui/GenreAddController.java
@@ -0,0 +1,13 @@
+package dk.easv.cs5.mytunes.gui;
+
+import javafx.event.ActionEvent;
+
+public class GenreAddController {
+    public void onCancelButton(ActionEvent actionEvent) {
+
+    }
+
+    public void onSaveButton(ActionEvent actionEvent) {
+
+    }
+}
